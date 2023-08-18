b1ee174a475f72c5feea6150a30873902083e9fd
Kevin Abrahamsson <kevin.abrahamsson@iths.se>
2023-08-18 12:02:53 +0200

    .

--- Exercises/Exercise6-cicd-tools/test_print_joke.py
+++ Exercises/Exercise6-cicd-tools/test_print_joke.py
@@ -3,13 +3,26 @@ from print_joke import get_random_reaction
 
 def test_get_random_reaction_type():
     reaction = get_random_reaction()
-    assert False # Replace False with a check that makes sure the reaction is a string type
+    assert type(reaction) == str
 
 
-def test_get_random_reaction_repeats():
-    # Write a test that checks that multiple calls to get_random_reaction()
-    # doesn't give you the same reaction every time
-    pass 
+def test_get_random_reaction_repeats():    
+    reaction1 = get_random_reaction()
+    reaction2 = reaction1
+    tries = 0
+    
+    while reaction1 == reaction2:
+        reaction1 = get_random_reaction()
+        
+        if tries >= 100:
+            raise Exception("FUCK")
+        
+        tries += 1
+        
+
 
 
 # Come up with a test of your own and implement it here.
+def test_print_random_joke_and_reaction_type():
+    reaction = get_random_reaction()
+    assert type(reaction) == str
